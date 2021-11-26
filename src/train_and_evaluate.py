2785f42c3af9fe64347662db86b6915cbc453447
unknown <ankitmarwahaa7@gmail.com>
2021-11-27 00:25:51 +0530

    tracker added

--- src/train_and_evaluate.py
+++ src/train_and_evaluate.py
@@ -16,20 +16,68 @@ import argparse
 import joblib
 import json
 
+
+def eval_metrics(actual, pred):
+    rmse = np.sqrt(mean_squared_error(actual, pred))
+    mae = mean_squared_error(actual, pred)
+    r2 = r2_score(actual, pred)
+    return rmse, mae, r2
+
+
 def train_and_evaluate(config_path):
-    config_path = read_params(config_path)
-    train_data_path = config_path["split_data"]["train_path"]
-    test_data_path = config_path["split_data"]["test_path"]
-    random_state =
-    model_dir =
-    alpha =
-    l1_ratio =
-    target =
+    config = read_params(config_path)
+    train_data_path = config["split_data"]["train_path"]
+    test_data_path = config["split_data"]["test_path"]
+    random_state = config["base"]["random_state"]
+    model_dir = config["model_dir"]
+    alpha = config["estimators"]["ElasticNet"]["params"]["alpha"]
+    l1_ratio = config["estimators"]["ElasticNet"]["params"]["l1_ratio"]
+    target = [config["base"]["target_col"]]
+
+    train = pd.read_csv(train_data_path, sep=",")
+    test = pd.read_csv(test_data_path, sep=",")
+
+    train_y = train[target]
+    test_y = test[target]
+
+    train_x = train.drop(target, axis=1)
+    test_x = test.drop(target, axis=1)
+
+    lr = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=random_state)
+
+    lr.fit(train_x, train_y)
+
+    predicted_qualities = lr.predict(test_x)
+
+    (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
+
+    print("Elasticnet model (alpha=%f, l1_ratio=%f):" % (alpha, l1_ratio))
+    print(" RMSE: %s" % rmse)
+    print(" MAE: %s" % mae)
+    print(" R2: %s" % r2)
+
+    scores_file = config["reports"]["scores"]
+    with open(scores_file, "w") as f:
+        scores = {
+            "rmse": rmse,
+            "mae": mae,
+            "r2": r2
+        }
+        json.dump(scores, f, indent=4)
+    params_file = config["reports"]["params"]
+    with open(params_file, "w") as f:
+        params = {
+            "alpha": alpha,
+            "l1_ratio": l1_ratio,
+        }
+        json.dump(params, f, indent=4)
+    os.makedirs(model_dir, exist_ok=True)
+    model_path = os.path.join(model_dir, "model.joblib")
+    joblib.dump(lr, model_path)
+
 
 if __name__ == "__main__":
     args = argparse.ArgumentParser()
     args.add_argument("--config", default="params.yaml")
     parsed_args = args.parse_args()
-    load_and_save(config_path=parsed_args.config)
-
-
+    train_and_evaluate(config_path=parsed_args.config)
